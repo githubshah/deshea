6ae77e6eb75899284eac69fee0ad214ba4551039
shaid hussain <shaid@gappsexperts.com>
2020-09-30 23:14:50 +0530

    create file

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,66 +1,58 @@
 
+import javax.sound.sampled.AudioFileFormat;
 import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.DataLine;
-import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.SourceDataLine;
-import javax.sound.sampled.TargetDataLine;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
-import java.net.InetAddress;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
 
 public class Main {
 
     final int serverPort = 9786;
-    int dataPacketSize = 4096;
-    ByteArrayOutputStream byteOutputStream;
-    AudioFormat adFormat;
-    TargetDataLine targetDataLine;
-    AudioInputStream inputStream;
-    SourceDataLine sourceLine;
-    Map<String, Integer> connectedClientsMap = new HashMap(); // [ip , port]
-
-    private AudioFormat getAudioFormat() {
-        float sampleRate = 8000.0F;
-        int sampleSizeInBits = 16;
-        int channels = 1;
-        boolean signed = true;
-        boolean bigEndian = true;
-        return new AudioFormat(sampleRate, sampleSizeInBits, channels, signed, bigEndian);
-    }
-
     DatagramSocket udpServerSocket;
+    ByteArrayOutputStream byteArrayOutputStream;
 
     public void runVOIP() {
         try {
             udpServerSocket = new DatagramSocket(serverPort);
             System.out.println("Server started on port: " + serverPort);
+            byteArrayOutputStream = new ByteArrayOutputStream();
+            byte[] tempBuffer = new byte[10000];
+
+            new Thread(() -> {
+                try {
+                    Thread.sleep(60000);
+                    System.out.println("Byte stream closed");
+                    byteArrayOutputStream.close();
+                    //playAudio();
+                    toFile();
+                } catch (InterruptedException | IOException e) {
+                    e.printStackTrace();
+                }
+            }).start();
+
+            ThreadPoolExecutor executor = (ThreadPoolExecutor) Executors.newFixedThreadPool(10);
+
+
             while (true) {
-                byte[] receiveData = new byte[dataPacketSize];
-                DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+                DatagramPacket receivePacket = new DatagramPacket(tempBuffer, tempBuffer.length);
+                System.out.println("Server waiting to retrieve: " + serverPort);
                 udpServerSocket.receive(receivePacket);
                 try {
-                    int callerPort = receivePacket.getPort();
-                    InetAddress callerInetAddress = receivePacket.getAddress();
-                    String callerIp = callerInetAddress.getHostAddress();
-
-                    if (!connectedClientsMap.containsKey(callerIp) || callerPort != connectedClientsMap.get(callerIp)) {
-                        System.out.println("New Client Connected : " +
-                            callerIp + ":" + receivePacket.getPort());
-                        connectedClientsMap.put(callerIp, callerPort);
-                    }
-
-                    new Thread(() -> {
-                        this.sendToClient(connectedClientsMap, receivePacket, callerIp, callerPort);
-                    }).start();
+                    System.out.println("write some thing");
+                    executor.execute(() -> {
+                        byteArrayOutputStream.write(tempBuffer, 0, tempBuffer.length);
+                    });
                 } catch (Exception e) {
                     System.out.println(e);
                     System.exit(0);
@@ -71,62 +63,111 @@ public class Main {
         }
     }
 
-    /**
-     * @param connectedClientsMap: [callerIp,callerPort]
-     *                             //* @param audioData
-     * @param callerIp:            caller ip
-     * @param callerPort:          caller port
-     */
-    private void sendToClient(Map<String, Integer> connectedClientsMap, DatagramPacket receivePacket, String callerIp, int callerPort) {
-        System.out.println("Connected User count: " + connectedClientsMap.size());
-        connectedClientsMap.forEach((connectedUserIp, connectedUserPort) -> {
-            try {
-                if (!connectedUserIp.equals(callerIp)) { // skip local host client
-                    Thread.yield();
-                    System.out.println("Packet send to ip: " + connectedUserIp + ", port: " + connectedUserPort);
-                    DatagramPacket sendPacket =
-                        new DatagramPacket(receivePacket.getData(),
-                            receivePacket.getData().length, InetAddress.getByName(connectedUserIp), connectedUserPort);
-                    udpServerSocket.send(sendPacket);
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        });
+    private void toFile() throws IOException {
+        File dstFile = new File("/shah/dst.wav");
+        FileOutputStream out = new FileOutputStream(dstFile);
+        byte audioData[] = byteArrayOutputStream.toByteArray();
+        InputStream byteArrayInputStream = new ByteArrayInputStream(audioData);
+        AudioInputStream leftoutputAIS = new AudioInputStream(byteArrayInputStream, getAudioFormat(), audioData.length / getAudioFormat().getFrameSize());
+        AudioSystem.write(leftoutputAIS, AudioFileFormat.Type.WAVE, dstFile);
+
     }
 
-    private void playHere(byte[] audioData) throws LineUnavailableException {
-        InputStream byteInputStream = new ByteArrayInputStream(audioData);
-        AudioFormat adFormat = getAudioFormat();
-        inputStream = new AudioInputStream(byteInputStream, adFormat, audioData.length / adFormat.getFrameSize());
-        DataLine.Info dataLineInfo = new DataLine.Info(SourceDataLine.class, adFormat);
-        sourceLine = (SourceDataLine) AudioSystem.getLine(dataLineInfo);
-        sourceLine.open(adFormat);
-        sourceLine.start();
-        Thread playThread = new Thread(new PlayThread());
-        playThread.start();
-    }
+    AudioInputStream audioInputStream;
+    SourceDataLine sourceDataLine;
 
-    public class PlayThread extends Thread {
+    //This method plays back the audio data that
+    // has been saved in the ByteArrayOutputStream
+    private void playAudio() {
+        try {
+            //Get everything set up for playback.
+            //Get the previously-saved data into a byte
+            // array object.
+            byte audioData[] = byteArrayOutputStream.
+                toByteArray();
+            //Get an input stream on the byte array
+            // containing the data
+            InputStream byteArrayInputStream =
+                new ByteArrayInputStream(audioData);
+            AudioFormat audioFormat = getAudioFormat();
+            audioInputStream = new AudioInputStream(
+                byteArrayInputStream,
+                audioFormat,
+                audioData.length / audioFormat.
+                    getFrameSize());
+            DataLine.Info dataLineInfo =
+                new DataLine.Info(
+                    SourceDataLine.class,
+                    audioFormat);
+            sourceDataLine = (SourceDataLine)
+                AudioSystem.getLine(dataLineInfo);
+            sourceDataLine.open(audioFormat);
+            sourceDataLine.start();
 
-        byte tempBuffer[] = new byte[dataPacketSize];
+            //Create a thread to play back the data and
+            // start it  running.  It will run until
+            // all the data has been played back.
+            Thread playThread = new PlayThread();
+            playThread.start();
+        } catch (Exception e) {
+            System.out.println(e);
+            System.exit(0);
+        }//end catch
+    }//end playAudio
+
+    class PlayThread extends Thread {
+        byte tempBuffer[] = new byte[10000];
 
         public void run() {
             try {
                 int cnt;
-                while ((cnt = inputStream.read(tempBuffer, 0, tempBuffer.length)) != -1) {
+                //Keep looping until the input read method
+                // returns -1 for empty stream.
+                while ((cnt = audioInputStream.read(
+                    tempBuffer, 0,
+                    tempBuffer.length)) != -1) {
                     if (cnt > 0) {
-                        sourceLine.write(tempBuffer, 0, cnt);
-                    }
-                }
+                        //Write data to the internal buffer of
+                        // the data line where it will be
+                        // delivered to the speaker.
+                        sourceDataLine.write(tempBuffer, 0, cnt);
+                    }//end if
+                }//end while
+                //Block and wait for internal buffer of the
+                // data line to empty.
+                sourceDataLine.drain();
+                sourceDataLine.close();
             } catch (Exception e) {
                 System.out.println(e);
                 System.exit(0);
-            }
-        }
-    }
+            }//end catch
+        }//end run
+    }//end inner class PlayThread
 
-    private static HashSet<Integer> portSet = new HashSet<Integer>();
+    //This method creates and returns an
+    // AudioFormat object for a given set of format
+    // parameters.  If these parameters don't work
+    // well for you, try some of the other
+    // allowable parameter values, which are shown
+    // in comments following the declartions.
+    private AudioFormat getAudioFormat() {
+        float sampleRate = 8000.0F;
+        //8000,11025,16000,22050,44100
+        int sampleSizeInBits = 16;
+        //8,16
+        int channels = 1;
+        //1,2
+        boolean signed = true;
+        //true,false
+        boolean bigEndian = false;
+        //true,false
+        return new AudioFormat(
+            sampleRate,
+            sampleSizeInBits,
+            channels,
+            signed,
+            bigEndian);
+    }//end getAudioFormat
 
     public static void main(String args[]) throws Exception {
         new Main().runVOIP();
