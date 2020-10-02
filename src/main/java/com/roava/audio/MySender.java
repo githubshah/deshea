b70fa96e496ad1fd84e8015684110feb76bb3cb3
shaid hussain <shaid@gappsexperts.com>
2020-10-02 23:18:22 +0530

    hd

--- src/main/java/com/roava/audio/MySender.java
+++ src/main/java/com/roava/audio/MySender.java
@@ -3,7 +3,10 @@ package com.roava.audio;
 import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.DataLine;
+import javax.sound.sampled.Line;
+import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.Mixer;
+import javax.sound.sampled.SourceDataLine;
 import javax.sound.sampled.TargetDataLine;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
@@ -28,14 +31,62 @@ public class MySender {
 //            false);
 
         //return new AudioFormat(44100.0f, 16, 2, true, true);
-        return new AudioFormat(8000.0f, 16, 1, true, true);
+        //return new AudioFormat(44100, 16, 4, true, true);
+        return new AudioFormat(AudioFormat.Encoding.PCM_SIGNED,
+            44100.0F,16,2,2*2, 44100.0f,true);
 
     }//end getAudioFormat
+
+
+    public void m1ain() throws LineUnavailableException {
+        Mixer mixer = AudioSystem.getMixer(null); // default mixer
+        mixer.open();
+
+        System.out.printf("Supported SourceDataLines of default mixer (%s):\n\n", mixer.getMixerInfo().getName());
+        for (Line.Info info : mixer.getSourceLineInfo()) {
+            if (SourceDataLine.class.isAssignableFrom(info.getLineClass())) {
+                SourceDataLine.Info info2 = (SourceDataLine.Info) info;
+                System.out.println(info2);
+                System.out.printf("  max buffer size: \t%d\n", info2.getMaxBufferSize());
+                System.out.printf("  min buffer size: \t%d\n", info2.getMinBufferSize());
+                javax.sound.sampled.AudioFormat[] formats = info2.getFormats();
+                System.out.println("  Supported Audio formats: ");
+                for (javax.sound.sampled.AudioFormat format : formats) {
+                    System.out.println("    " + format);
+                    // System.out.printf(" encoding: %s\n", format.getEncoding());
+                    // System.out.printf(" channels: %d\n", format.getChannels());
+                    // System.out.printf(format.getFrameRate()==-1?"":" frame rate [1/s]: %s\n",
+                    // format.getFrameRate());
+                    // System.out.printf(" frame size [bytes]: %d\n", format.getFrameSize());
+                    // System.out.printf(format.getSampleRate()==-1?"":" sample rate [1/s]: %s\n",
+                    // format.getSampleRate());
+                    // System.out.printf(" sample size [bit]: %d\n", format.getSampleSizeInBits());
+                    // System.out.printf(" big endian: %b\n", format.isBigEndian());
+                    //
+                    // Map<String,Object> prop = format.properties();
+                    // if(!prop.isEmpty()) {
+                    // System.out.println(" Properties: ");
+                    // for(Map.Entry<String, Object> entry : prop.entrySet()) {
+                    // System.out.printf(" %s: \t%s\n", entry.getKey(), entry.getValue());
+                    // }
+                    // }
+                }
+                System.out.println();
+            } else {
+                System.out.println(info.toString());
+            }
+            System.out.println();
+        }
+
+        mixer.close();
+
+    }
 
     public void captureAudio() {
         try {
             //Get and display a list of
             // available mixers.
+            m1ain();
             Mixer.Info[] mixerInfo = AudioSystem.getMixerInfo();
             System.out.println("Available mixers:");
             for (int cnt = 0; cnt < mixerInfo.length; cnt++) {
@@ -62,6 +113,8 @@ public class MySender {
             targetDataLine.open(audioFormat);
             targetDataLine.start();
 
+
+
             //Create a thread to capture the microphone
             // data and start it running.  It will run
             // until the Stop button is clicked.
@@ -75,7 +128,7 @@ public class MySender {
 
     class CaptureThread extends Thread {
         //An arbitrary-size temporary holding buffer
-        byte tempBuffer[] = new byte[10000];
+        byte tempBuffer[] = new byte[40000];
 
         public void run() {
             DatagramSocket clientSenderSocket = null;
