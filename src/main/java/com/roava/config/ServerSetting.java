feffa6c28ec67f065ded9cb82236f8b94eecc339
shaid hussain <shaid@gappsexperts.com>
2020-10-05 16:27:07 +0530

    change port and buffer size

--- src/main/java/com/roava/config/ServerSetting.java
+++ src/main/java/com/roava/config/ServerSetting.java
@@ -3,5 +3,5 @@ package com.roava.config;
 public class ServerSetting {
     public final static String SERVER_IP = "3.16.216.89";
     //public final static String SERVER_IP = "localhost";
-    public final static int SERVER_PORT = 9786;
+    public final static int SERVER_PORT = 9785;
 }
