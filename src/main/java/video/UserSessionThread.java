7f39ec05613e2c5f96c7a3f338e31c0b1f9f5e43
shaid hussain <shaid@gappsexperts.com>
2020-10-11 13:31:49 +0530

    connect established

--- src/main/java/video/UserSessionThread.java
+++ src/main/java/video/UserSessionThread.java
@@ -25,34 +25,35 @@ public class UserSessionThread extends Thread {
                 DataInputStream din = new DataInputStream(socket.getInputStream());
                 String msg = din.readUTF();
                 MessagePacket messagePacket = new Gson().fromJson(msg, MessagePacket.class);
+                String type = messagePacket.getType();
                 switch (messagePacket.getEvent()) {
                     case Constants.CONNECT:
                         this.email = messagePacket.getEmail();
                         this.ip = socket.getInetAddress().getHostName();
                         this.type = messagePacket.getType();
-                        System.out.println("Request Event: Constants.CONNECT " + email + " : " + ip);
+                        System.out.println("type: " + type + " ," + "Request Event: Constants.CONNECT " + email + " : " + ip);
                         server.createSession(email, ip);
                         break;
                     case Constants.GET_PATIENTS:
-                        System.out.println("Request Event: Constants.GET_PATIENTS " + email + " : " + ip);
+                        System.out.println("type: " + type + " ," + "Request Event: Constants.GET_PATIENTS " + email + " : " + ip);
                         DataOutputStream patientList = new DataOutputStream(socket.getOutputStream());
                         patientList.writeUTF(new Gson().toJson(
                             new MessagePacket(email, Constants.GET_PATIENTS, "receptionist").setEmailPojos(server.getPatientList())));
                         break;
                     case Constants.GET_RECEPTIONISTS:
-                        System.out.println("Request Event: Constants.GET_RECEPTIONISTS " + email + " : " + ip);
+                        System.out.println("type: " + type + " ," + "Request Event: Constants.GET_RECEPTIONISTS " + email + " : " + ip);
                         DataOutputStream receptionistList = new DataOutputStream(socket.getOutputStream());
                         receptionistList.writeUTF(new Gson().toJson(
                             new MessagePacket(email, Constants.GET_RECEPTIONISTS, "receptionist").setEmailPojos(server.getReceptionistList())));
                         break;
                     case Constants.GET_CONNECTIONS:
-                        System.out.println("Request Event: Constants.CONNECTIONS " + email + " : " + ip);
+                        System.out.println("type: " + type + " ," + "Request Event: Constants.CONNECTIONS " + email + " : " + ip);
                         DataOutputStream connectionList = new DataOutputStream(socket.getOutputStream());
                         connectionList.writeUTF(new Gson().toJson(
                             new MessagePacket(email, Constants.GET_CONNECTIONS, "receptionist").setConnection(server.getConnectionList())));
                         break;
                     case Constants.CONNECT_TO:
-                        System.out.println("Request Event: Constants.CONNECT_TO " + email + " : " + ip);
+                        System.out.println("type: " + type + " ," + "Request Event: Constants.CONNECT_TO " + email + " : " + ip);
                         this.ip = socket.getInetAddress().getHostName();
                         Socket patientSocket = server.connectTo(ip, messagePacket.getConnectTo());
                         MessagePacket connect = new MessagePacket("sahid@gmail.com", "receptionistavailable", "patient");
@@ -60,18 +61,18 @@ public class UserSessionThread extends Thread {
                         receptionAvailable.writeUTF(new Gson().toJson(connect));
                         break;
                     case Constants.CONNECT_TO_RECEPTIONIST:
-                        System.out.println("Request Event: Constants.CONNECT_TO_RECEPTIONIST " + email + " : " + ip);
+                        System.out.println("type: " + type + " ," + "Request Event: Constants.CONNECT_TO_RECEPTIONIST " + email + " : " + ip);
                         this.email = messagePacket.getEmail();
                         this.ip = socket.getInetAddress().getHostName();
                         server.createSession(email, ip);
                         if (server.hasConnection(ip)) {
                             MessagePacket connect124 =
-                                new MessagePacket("sahid@gmail.com", Constants.RECEPTIONIST_AVAILABLE, "patient");
+                                new MessagePacket(messagePacket.email, Constants.RECEPTIONIST_AVAILABLE, type);
                             DataOutputStream receptionist_Available = new DataOutputStream(socket.getOutputStream());
                             receptionist_Available.writeUTF(new Gson().toJson(connect124));
                         } else {
                             MessagePacket connect13 =
-                                new MessagePacket("sahid@gmail.com", Constants.RECEPTIONIST_NOT_AVAILABLE, "patient");
+                                new MessagePacket(messagePacket.email, Constants.RECEPTIONIST_NOT_AVAILABLE, type);
                             DataOutputStream dout13 = new DataOutputStream(socket.getOutputStream());
                             dout13.writeUTF(new Gson().toJson(connect13));
                         }
