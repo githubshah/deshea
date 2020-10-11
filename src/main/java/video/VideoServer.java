14850110b0aaaf724413566801ab983bafb7e354
shaid hussain <shaid@gappsexperts.com>
2020-10-11 21:05:06 +0530

    prashant ip

--- src/main/java/video/VideoServer.java
+++ src/main/java/video/VideoServer.java
@@ -21,7 +21,7 @@ public class VideoServer {
     private Map<String, String> activeUserMap = new HashMap(); // email, ip
 
     public void openVideoServerSocket() {
-        conferenceMap.put("59.89.53.93", "47.31.208.193");
+        conferenceMap.put("59.89.53.93", "103.92.42.226");
         try (ServerSocket serverSocket = new ServerSocket(9898)) {
             System.out.println("Chat Server is listening on port " + 9898);
             while (true) {
@@ -190,7 +190,7 @@ public class VideoServer {
     public Map<String, String> getConnectionList() {
         Map<String, String> map = new HashMap<>();
         System.out.println("size of confer======== " + conferenceMap.size());
-        map.put("59.89.53.93", "47.31.208.193");
+        map.put("59.89.53.93", "103.92.42.226");
         System.out.println("size of map: " + map.size());
         return map;
     }
